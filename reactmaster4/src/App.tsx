1c8892e6e35fcc14498d47c022254a1abe870ad2
Jun-Young Noh <119721103+junyoungnnn@users.noreply.github.com>
2024-06-11 23:08:55 +0900

    dragdrop

---
+++ reactmaster4/src/App.tsx
@@ -0,0 +1,35 @@
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+
+function App() {
+  const onDragEnd = () => {};
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div>
+        <Droppable droppableId="one">
+          {(magic) => (
+            <ul ref={magic.innerRef} {...magic.droppableProps}>
+              <Draggable draggableId="first" index={0}>
+                {(magic) => (
+                  <li ref={magic.innerRef} {...magic.draggableProps}>
+                    <span {...magic.dragHandleProps}>❤</span>
+                    One
+                  </li>
+                )}
+              </Draggable>
+              <Draggable draggableId="second" index={1}>
+                {(magic) => (
+                  <li ref={magic.innerRef} {...magic.draggableProps}>
+                    <span {...magic.dragHandleProps}>🌹</span>
+                    Two
+                  </li>
+                )}
+              </Draggable>
+            </ul>
+          )}
+        </Droppable>
+      </div>
+    </DragDropContext>
+  );
+}
+
+export default App;
